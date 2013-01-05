c86f2cc809908e675391102fb3c316726282ecdb
Carlo Andaya <carlo@carloandaya.com>
2013-01-05 15:44:29 -0800

    Stubbed out ListViewController

---
+++ Nerdfeed/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  Nerdfeed
+//
+//  Created by Carlo Andaya on 1/5/13.
+//  Copyright (c) 2013 Carlo Andaya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ListViewController : UITableViewController
+
+@end
---
+++ Nerdfeed/ListViewController.m
@@ -0,0 +1,23 @@
+//
+//  ListViewController.m
+//  Nerdfeed
+//
+//  Created by Carlo Andaya on 1/5/13.
+//  Copyright (c) 2013 Carlo Andaya. All rights reserved.
+//
+
+#import "ListViewController.h"
+
+@implementation ListViewController
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+@end
