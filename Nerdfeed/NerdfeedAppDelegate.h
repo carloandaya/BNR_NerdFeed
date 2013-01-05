3e8f73883c512887f0af354cb7762a56ab0ae472
Carlo Andaya <carlo@carloandaya.com>
2013-01-05 15:43:59 -0800

    Project created.

---
+++ Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Carlo Andaya on 1/5/13.
+//  Copyright (c) 2013 Carlo Andaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Carlo Andaya on 1/5/13.
+//  Copyright (c) 2013 Carlo Andaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+    }
+}
